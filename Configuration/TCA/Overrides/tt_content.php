222c03b9dd77e93741ac392498130bf076491404
Maximilian Faessler <maximilian@faesslerweb.de>
2021-02-09 10:19:25 +0000

    [EDIT] ext_tables update

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-//=================================================================
+///=================================================================
 // Register Plugins
 //=================================================================
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
@@ -9,32 +9,35 @@ defined('TYPO3_MODE') || die('Access denied.');
     'StandardForm',
     'RKW Form: Standard Formular'
 );
-
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'RKW.RkwForm',
     'BstForm',
-    'RKW Form: BST Formular'
+    'RKW Form: Bausachverständigentag Digital'
 );
+
+
 
 //=================================================================
 // Add Flexform
 //=================================================================
-
 $extKey = 'rkw_form';
 
-// add here all form plugins, which should use this standard flexform
-$pluginList = ['StandardForm', 'BstForm'];
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
+$pluginName = strtolower('StandardForm');
+$pluginSignature = $extensionName.'_'.$pluginName;
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:'. $extKey . '/Configuration/FlexForms/Standard.xml'
+);
 
-foreach ($pluginList as $plugin) {
-    $extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
-    $pluginName = strtolower($plugin);
-    $pluginSignature = $extensionName.'_'.$pluginName;
-
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-        $pluginSignature,
-        'FILE:EXT:'. $extKey . '/Configuration/FlexForms/Standard.xml'
-    );
-}
-
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
+$pluginName = strtolower('BstForm');
+$pluginSignature = $extensionName.'_'.$pluginName;
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:'. $extKey . '/Configuration/FlexForms/Standard.xml'
+);
